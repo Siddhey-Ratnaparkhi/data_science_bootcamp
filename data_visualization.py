c23ad3525275d5844f6539e267b1648d94a20a1e
siddhey-ratnaparkhi <siddheyratnaparkhi15@gmail.com>
2021-08-21 12:57:32 +0200

    add visualization functions and modularize code

--- data_visualization.py
+++ data_visualization.py
@@ -1,58 +1,75 @@
-# import pands, numpy, pandas_datareader to get bank data from yahoo finance data
-from pandas_datareader import data as web
+# import fetch_yahoo_finance_data, pandas, seaborn, matplotlib
+from fetch_yahoo_finance_data import bank_stocks, returns, tickers
 import pandas as pd
-import datetime
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# define start date and end date
-start = datetime.datetime(2018, 1, 1)
-end = datetime.datetime(2021, 6, 1)
 
-# Bank of America
-BAC = web.DataReader("BAC", 'yahoo', start, end)
+def main():
+    # select function name amongst: Pair Plot, Distplot, Lineplot, Plotly Lineplot, Moving Average, Heatmap, Clustermap
+    function_name = 'SELECT FUNCTION NAME OF THE PLOT'
 
-# CitiGroup
-C = web.DataReader("C", 'yahoo', start, end)
+    if function_name == 'Pair Plot':
+        pairplot()
+    elif function_name == 'Distplot':
+        distplot()
+    elif function_name == 'Lineplot':
+        lineplot()
+    elif function_name == 'Plotly Lineplot':
+        plotly_lineplot()
+    elif function_name == 'Moving Average':
+        moving_average()
+    elif function_name == 'Heatmap':
+        heatmap()
+    elif function_name == 'Clustermap':
+        clustermap()
+    else:
+        print('Input valid function name of visualization plot: Pair Plot, Distplot, Lineplot, Plotly Lineplot, Moving Average, Heatmap, Clustermap')
 
-# Goldman Sachs
-GS = web.DataReader("GS", 'yahoo', start, end)
-
-# JPMorgan Chase
-JPM = web.DataReader("JPM", 'yahoo', start, end)
-
-# Morgan Stanley
-MS = web.DataReader("MS", 'yahoo', start, end)
-
-# Wells Fargo
-WFC = web.DataReader("WFC", 'yahoo', start, end)
-
-# list of ticker symbols in alphabetical order
-tickers = ['BAC', 'C', 'GS', 'JPM', 'MS', 'WFC']
-
-# concatenate all stocks together
-bank_stocks = pd.concat([BAC, C, GS, JPM, MS, WFC], axis=1, keys=tickers)
-
-# print(bank_stocks.tail(10))
-
-# set column name levels
-bank_stocks.columns.names = ['Bank Ticker Name', 'Stock Information']
-
-# print(bank_stocks.tail(10))
-
-# EDA: Explorative Data Analysis
-
-# get minimum closing price for all stocks from banks over the given time-frame
-minimum_closing_price = bank_stocks.xs(key='Close', axis=1, level='Stock Information').min()
-# print(minimum_closing_price)
-
-# get returns for each bank's stock using percentage change between current and prior element
-returns = pd.DataFrame()
-for tick in tickers:
-    returns[tick + ' Return'] = bank_stocks[tick]['Close'].pct_change()
-
-# print(returns.head(10))
 
 # create a pairplot using seaborn
-sns.pairplot(returns[1:])
-plt.show()
+def pairplot():
+    sns.pairplot(returns[1:])
+    return plt.show()
+
+# create distplot for the year 2020 returns for JPMorgan Chase
+def distplot():
+    sns.distplot(returns.loc['2020-01-01':'2020-12-31']['JPM Return'], color='blue', bins=50)
+    return plt.show()
+
+# create line plot for the Close Price over the entire period for each bank
+def lineplot():
+    df = bank_stocks.xs(key='Close',axis=1,level='Stock Information')
+    df.plot()
+    return plt.show()
+
+# create line plot using plotly for gaining more insights over specific time-frame
+def plotly_lineplot():
+    pd.options.plotting.backend = "plotly"
+    df = bank_stocks.xs(key='Close',axis=1,level='Stock Information')
+    fig = df.plot.area()
+    return fig.show()
+
+# 30 day moving average for JPMorgan Chase stocks during time-frame 01.Jan 2020 to 01.Jan 2021
+def moving_average():
+    plt.figure(figsize=(12,6))
+    bank_stocks['JPM']['Close'].loc['2020-01-01':'2021-01-01'].rolling(window=30).mean().plot(label='30 Day Moving Average')
+    bank_stocks['JPM']['Close'].loc['2020-01-01':'2021-01-01'].plot(label='JPM CLOSE')
+    plt.legend()
+    return plt.show()
+
+# create heat-map of correlation between the Close Price of stocks
+def heatmap():
+    sns.heatmap(bank_stocks.xs(key='Close', axis=1, level='Stock Information').corr(), annot=True)
+    return plt.show()
+
+# create cluster-map of correlation between the Close Price of stocks
+def clustermap():
+    sns.clustermap(bank_stocks.xs(key='Close', axis=1, level='Stock Information').corr(), annot=True)
+    return plt.show()
+
+
+
+
+if __name__ == "__main__":
+    main()
